b568dd6e25cf37905d0fd1fd6f860e130282eb1c
paata4453 <cucqiridzepaata2@gmail.com>
2025-06-18 17:03:09 +0400

    commit

--- cw.js
+++
@@ -1,3 +0,0 @@
-const {
-  arr: [{ level: [level1_1, level1_2, level1_3] }, {level: [level2_1, level2_2, level2_3], students: [{name: name1,surname: surname1,score: [score1_1, score1_2, score1_3], },{name: name2,surname: surname2,score: [score2_1, score2_2, score2_3], },],},],
-} = arr;
